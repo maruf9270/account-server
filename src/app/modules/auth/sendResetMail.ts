e0773b7ec91cf8db41fe75e3fda21a9b0f7bac48
Maruf-9270 <marufahmed9270@gmail.com>
2025-04-27 11:58:56 +0600

    Added email

--- src/app/modules/auth/sendResetMail.ts
+++ src/app/modules/auth/sendResetMail.ts
@@ -3,9 +3,9 @@ import config from "../../config";
 
 export async function sendEmail(to: string, html: string) {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    host: "mail.smtrustbd.com",
+    port: 465,
+    secure: true,
     auth: {
       user: config.email,
       pass: config.appPass,
